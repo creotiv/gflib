786715e3c512da28be6510ef50fa2f1dfb457b5f
Andrey Nikishaev <creotiv@gmail.com>
2011-08-25 18:59:49 +0300

    Added HTTP and AMF Controllers

--- modules/News/User.py
+++ modules/News/User.py
@@ -1,6 +1,6 @@
 from gflib.parsing import json_encode
 
-class UserController(object):
+class UserHTTPController(object):
 
     def __init__(self,env,response,request,headers):
         self.env      = env
@@ -10,4 +10,13 @@ class UserController(object):
         
     def IndexAction(self):
         self.response('200 OK', self.headers )
+        return [json_encode(self.request)]
+        
+class UserAMFController(object):
+
+    def __init__(self,env,request):
+        self.env      = env
+        self.request  = request
+        
+    def IndexAction(self):
         return [json_encode(self.request)]
