082256712bcc05cdb8d0a76c4fd3d9e6574e42c9
Andrey Nikishaev <creotiv@gmail.com>
2011-08-27 18:30:07 +0300

    Added distributed http router

--- gflib/network/protocols/__init__.py
+++ gflib/network/protocols/__init__.py
@@ -1,5 +1,5 @@
 from amf import AMFProtocol,AMFDOTRouter
-from http import HTTPProtocol,HTTPDOTRouter
+from http import HTTPProtocol,HTTPDOTRouter,DistributedHTTPDOTRouter
 #from socketio import SocketIOProtocol
 
 
